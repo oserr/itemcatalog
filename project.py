d55bbaad86811410d7835a1129633a59da1b1544
Omar Serrano <oser520@gmail.com>
2017-07-02 19:21:17 -0700

    Define gensalt() and get_hash()

--- project.py
+++ project.py
@@ -1,11 +1,43 @@
 #!/usr/bin/env python
 # project.py
+import hmac
+import string
 from flask import (Flask, render_template,
                    request, redirect, url_for, flash,
                    send_from_directory)
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import User, Category, Item, Base
+
+
+def gensalt(length=16):
+    """Generate a random salt value for a password.
+
+    :param length
+        The lenght of the salt value, with default value of 16.
+    :return
+        A string containing a randomly generated salt value composed of
+        alphanumeric characters.
+    """
+    if not length or length < 0:
+        raise ValueError('The salt length must be a positive integer')
+    alnum = string.ascii_letters + string.digits
+    return ''.join(random.choice(alnum) for _ in range(length))
+
+
+def get_hash(salt, psswd):
+    """Create a hash from a salt and password.
+
+    :param salt
+        The salt value. Cannot be empty.
+    :param psswd
+        The password value. Cannot be empty.
+    :return
+        A hash value of the salt and password.
+    """
+    if not salt or not psswd:
+        raise ValueError('The salt and password cannot be empty')
+    return hmac.new(salt.encode(), psswd).hexdigest()
 
 
 def get_session_status():
